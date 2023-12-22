b00804fe0f7e065ca7c3b71cb069dc76b2504eee
Javier Alejandro Arteaga Gonzales <132113806+javierarteagagonzales@users.noreply.github.com>
2023-12-22 18:17:47 -0500

    Calculo de impuestos

---
+++ vueejerc/tareas/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <ComputedExample />
+</template>
+
+<script lang="ts" setup>
+import ComputedExample from './components/ComputedExample.vue'
+</script>
+
+<style>
+
+</style>
---
+++ vueejerc/tareas/src/components/ComputedExample.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <label>Subtotal: </label>
+        <input type="number" v-model="subtotal" />
+        <h3>Total impuestos a pagar: {{ totalVat }} ({{ vat }}%)</h3>
+    </div>
+</template>
+<script lang="ts" setup>
+import { Ref, ref ,computed} from "vue";
+
+/*
+CALCULO DE IMPUESTOS
+*/
+    const vat = 21
+    let subtotal:Ref<number> = ref(0)
+
+    const totalVat = computed(()=> vat*subtotal.value/100)
+</script>
+<style scoped>
+</style>
