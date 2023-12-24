730e0a8b73adaac2ed49e362412b70e2c837dbb3
Javier Alejandro Arteaga Gonzales <132113806+javierarteagagonzales@users.noreply.github.com>
2023-12-24 17:23:59 -0500

    consumiento API para listar posts

---
+++ vueejerc/services/src/components/PostList.vue
@@ -0,0 +1,28 @@
+<template>
+<h1>Listado de posts</h1>
+<ul class="post-list">
+    <li v-for="post in posts" :key="post.id"> {{ post.title }}</li>
+</ul>
+</template>
+
+<script lang="ts">
+//composition API
+    import { defineComponent, onMounted } from 'vue';
+    import PostService from '@/services/PostService'
+
+    export default defineComponent({
+        name: 'PostList',
+        setup() {
+            const service = new PostService()
+            const posts = service.getPosts()
+
+            onMounted(async () => {
+                await service.fetchAll()
+            })
+            return{posts}
+        }
+    })
+</script>
+
+<style scoped>
+</style>
